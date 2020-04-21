01512f16e4b7b0251afa41d977c8cf6008eddf32
gogbajbobo <grigoriev.maxim@gmail.com>
2020-04-21 09:47:57 +0300

    initial commit

---
+++ ml_bin.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
