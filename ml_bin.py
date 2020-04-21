9606c9fc0152a823106967db25d441384bb59ae8
gogbajbobo <grigoriev.maxim@gmail.com>
2020-04-21 18:43:35 +0300

    refactor / calc filtered and diff images

--- ml_bin.py
+++ ml_bin.py
@@ -21,29 +21,45 @@
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage import filters
+from skimage import morphology
 
 # %%
 sample_path = '/Users/grimax/Desktop/tmp/porous sample/tomo_rec_cut.h5'
 sample_file = h5py.File(sample_path, mode='r')
 sample = sample_file['Reconstruction']
-sample_min = np.min(sample)
-sample_max = np.max(sample)
-sample_mean = np.mean(sample)
-sample_std = np.std(sample)
-print(f'shape: {sample.shape}')
-print(f'min: {sample_min:.2f}')
-print(f'max: {sample_max:.2f}')
-print(f'mean: {sample_mean:.2f}')
-print(f'std: {sample_std:.2f}')
 
 
 # %%
-def show_histogram(image):
+def image_stats(image):
+    im_shape = image.shape
+    im_min = np.min(image)
+    im_max = np.max(image)
+    im_mean = np.mean(image)
+    im_std = np.std(image)
+    print(f'shape: {im_shape}')
+    print(f'min: {im_min:.2f}')
+    print(f'max: {im_max:.2f}')
+    print(f'mean: {im_mean:.2f}')
+    print(f'std: {im_std:.2f}')
+    return im_shape, im_min, im_max, im_mean, im_std
+
+
+# %%
+sample_shape, sample_min, sample_max, sample_mean, sample_std = image_stats(sample)
+
+
+# %%
+def plot_histogram(data, xmin=None, xmax=None, log=False):
+    plt.figure(figsize=(15, 5))
+    plt.hist(data, bins=255, color='lightgray', log=log)
+    xmin and xmax and plt.xlim(xmin, xmax)
+
+
+def get_histogram(image, xmin=None, xmax=None):
     img = np.ravel(image)
-    plt.figure(figsize=(15, 5))
-    plt.hist(img, bins=255, color='lightgray')
-    plt.figure(figsize=(15, 5))
-    plt.hist(img, bins=255, color='lightgray', log=True)
+    plot_histogram(img, xmin, xmax)
+    plot_histogram(img, xmin, xmax, True)
 
 
 # %%
@@ -66,9 +82,47 @@ def show_2d_sections(image, x=0, y=0, z=0):
 
 
 # %%
-show_histogram(sample)
+def plot_column(image, x=0, y=0):
+    plt.figure(figsize=(15, 5))
+    plt.plot(image[:, y, x])
+
 
 # %%
-show_2d_sections(sample, x=150, z=25)
+get_histogram(sample, xmin=sample_min, xmax=sample_max)
+
+# %%
+x_slice = 150
+y_slice = 0
+z_slice = 25
+show_2d_sections(sample, x=x_slice, y=y_slice, z=z_slice)
+
+# %%
+plot_column(sample)
+
+# %%
+sample_filtered = filters.median(sample, morphology.cube(5))
+sample_f_shape, sample_f_min, sample_f_max, sample_f_mean, sample_f_std = image_stats(sample_filtered)
+
+# %%
+get_histogram(sample_filtered, xmin=sample_f_min, xmax=sample_f_max)
+
+# %%
+show_2d_sections(sample_filtered, x=x_slice, y=y_slice, z=z_slice)
+
+# %%
+plot_column(sample_filtered)
+
+# %%
+sample_diff = sample - sample_filtered
+sample_diff_shape, sample_diff_min, sample_diff_max, sample_diff_mean, sample_diff_std = image_stats(sample_diff)
+
+# %%
+get_histogram(sample_diff, xmin=sample_diff_min, xmax=sample_diff_max)
+
+# %%
+show_2d_sections(sample_diff, x=x_slice, y=y_slice, z=z_slice)
+
+# %%
+plot_column(sample_diff)
 
 # %%
