22139f013acff0776c959641f4def6d796e00ef1
gogbajbobo <grigoriev.maxim@gmail.com>
2020-05-04 11:08:55 +0300

    Update recon_compare.py

--- recon_compare.py
+++ recon_compare.py
@@ -21,166 +21,85 @@
 import helper
 import anisotropic_volume_generator as avg
 import model_of_experiment as moe
+import matplotlib.pyplot as plt
 
 # %%
 size = 250
 dim = 3
 shape = tuple(size for _ in range(dim))
-phantom = avg.blobs(shape=shape, porosity=0.7)
+phantom = avg.blobs(shape=shape, porosity=0.5, random_seed=3)
+phantom = helper.fill_floating_solids_and_closed_pores(phantom)
 
 # %%
 sample_shape, sample_min, sample_max, sample_mean, sample_std = helper.get_stats(phantom)
 
 # %%
-x_slice = 125
-y_slice = 125
-z_slice = 125
+x_slice = size // 2
+y_slice = size // 2
+z_slice = size // 2
 
 helper.show_2d_sections(phantom, x=x_slice, y=y_slice, z=z_slice)
 
 # %%
-phantom_recon_45 = moe.process_image(phantom, 45)
-phantom_recon_90 = moe.process_image(phantom, 90)
-phantom_recon_180 = moe.process_image(phantom, 180)
+phantom_recon_180_no_filter = moe.process_image(phantom, 180)
+helper.show_2d_sections(phantom_recon_180_no_filter, x=x_slice, y=y_slice, z=z_slice)
 
-# %%
-helper.show_2d_sections(phantom_recon_45, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180, x=x_slice, y=y_slice, z=z_slice)
+reconstruct_filter='hamming'
+phantom_recon_180_hamming = moe.process_image(phantom, 180, reconstruct_filter=reconstruct_filter)
+helper.show_2d_sections(phantom_recon_180_hamming, x=x_slice, y=y_slice, z=z_slice)
 
-# %%
-reconstruct_filter='ramp'
-phantom_recon_45_ramp = moe.process_image(phantom, 45, reconstruct_filter=reconstruct_filter)
-phantom_recon_90_ramp = moe.process_image(phantom, 90, reconstruct_filter=reconstruct_filter)
-phantom_recon_180_ramp = moe.process_image(phantom, 180, reconstruct_filter=reconstruct_filter)
-
-# %%
-helper.show_2d_sections(phantom_recon_45_ramp, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_ramp, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_ramp, x=x_slice, y=y_slice, z=z_slice)
+reconstruct_sart=True
+phantom_recon_180_sart = moe.process_image(phantom, 180, reconstruct_sart=reconstruct_sart)
+helper.show_2d_sections(phantom_recon_180_sart, x=x_slice, y=y_slice, z=z_slice)
 
 # %%
 noise_method = 'poisson'
-noise_parameter = 100
-phantom_recon_45_ramp_noise_100 = moe.process_image(
-    phantom, 45, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_ramp_noise_100 = moe.process_image(
-    phantom, 90, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_180_ramp_noise_100 = moe.process_image(
-    phantom, 180, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-
-helper.show_2d_sections(phantom_recon_45_ramp_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_ramp_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_ramp_noise_100, x=x_slice, y=y_slice, z=z_slice)
-
-
-reconstruct_filter='shepp-logan'
-phantom_recon_45_shepp_logan = moe.process_image(phantom, 45, reconstruct_filter=reconstruct_filter)
-phantom_recon_90_shepp_logan = moe.process_image(phantom, 90, reconstruct_filter=reconstruct_filter)
-phantom_recon_180_shepp_logan = moe.process_image(phantom, 180, reconstruct_filter=reconstruct_filter)
-
-helper.show_2d_sections(phantom_recon_45_shepp_logan, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_shepp_logan, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_shepp_logan, x=x_slice, y=y_slice, z=z_slice)
-
-phantom_recon_45_shepp_logan_noise_100 = moe.process_image(
-    phantom, 45, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_shepp_logan_noise_100 = moe.process_image(
-    phantom, 90, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_180_shepp_logan_noise_100 = moe.process_image(
-    phantom, 180, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-
-helper.show_2d_sections(phantom_recon_45_shepp_logan_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_shepp_logan_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_shepp_logan_noise_100, x=x_slice, y=y_slice, z=z_slice)
-
-
-reconstruct_filter='hamming'
-phantom_recon_45_hamming = moe.process_image(phantom, 45, reconstruct_filter=reconstruct_filter)
-phantom_recon_90_hamming = moe.process_image(phantom, 90, reconstruct_filter=reconstruct_filter)
-phantom_recon_180_hamming = moe.process_image(phantom, 180, reconstruct_filter=reconstruct_filter)
-
-helper.show_2d_sections(phantom_recon_45_hamming, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_hamming, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_hamming, x=x_slice, y=y_slice, z=z_slice)
-
-phantom_recon_45_hamming_noise_100 = moe.process_image(
-    phantom, 45, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_hamming_noise_100 = moe.process_image(
-    phantom, 90, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_180_hamming_noise_100 = moe.process_image(
-    phantom, 180, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-
-helper.show_2d_sections(phantom_recon_45_hamming_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_hamming_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_hamming_noise_100, x=x_slice, y=y_slice, z=z_slice)
-
-
-# %%
-reconstruct_sart=True
-
-phantom_recon_45_sart = moe.process_image(phantom, 45, reconstruct_sart=reconstruct_sart)
-phantom_recon_90_sart = moe.process_image(phantom, 90, reconstruct_sart=reconstruct_sart)
-phantom_recon_180_sart = moe.process_image(phantom, 180, reconstruct_sart=reconstruct_sart)
-
-helper.show_2d_sections(phantom_recon_45_sart, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_sart, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_sart, x=x_slice, y=y_slice, z=z_slice)
-
-phantom_recon_45_sart_noise_100 = moe.process_image(
-    phantom, 45, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_sart_noise_100 = moe.process_image(
-    phantom, 90, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_180_sart_noise_100 = moe.process_image(
-    phantom, 180, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
-)
-
-helper.show_2d_sections(phantom_recon_45_sart_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_sart_noise_100, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_180_sart_noise_100, x=x_slice, y=y_slice, z=z_slice)
-
-# %%
 noise_parameter = 30
 
-phantom_recon_45_hamming_noise_30 = moe.process_image(
-    phantom, 45, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_hamming_noise_30 = moe.process_image(
-    phantom, 90, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
-)
+# %%
+phantom_recon_180_no_filter_noise_30 = moe.process_image(phantom, 180, noise_parameter=noise_parameter, noise_method=noise_method)
+helper.show_2d_sections(phantom_recon_180_no_filter_noise_30, x=x_slice, y=y_slice, z=z_slice)
+
 phantom_recon_180_hamming_noise_30 = moe.process_image(
     phantom, 180, reconstruct_filter=reconstruct_filter, noise_parameter=noise_parameter, noise_method=noise_method
 )
-
-helper.show_2d_sections(phantom_recon_45_hamming_noise_30, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_hamming_noise_30, x=x_slice, y=y_slice, z=z_slice)
 helper.show_2d_sections(phantom_recon_180_hamming_noise_30, x=x_slice, y=y_slice, z=z_slice)
 
-
-phantom_recon_45_sart_noise_30 = moe.process_image(
-    phantom, 45, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
-)
-phantom_recon_90_sart_noise_30 = moe.process_image(
-    phantom, 90, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
-)
 phantom_recon_180_sart_noise_30 = moe.process_image(
     phantom, 180, reconstruct_sart=reconstruct_sart, noise_parameter=noise_parameter, noise_method=noise_method
 )
-
-helper.show_2d_sections(phantom_recon_45_sart_noise_30, x=x_slice, y=y_slice, z=z_slice)
-helper.show_2d_sections(phantom_recon_90_sart_noise_30, x=x_slice, y=y_slice, z=z_slice)
 helper.show_2d_sections(phantom_recon_180_sart_noise_30, x=x_slice, y=y_slice, z=z_slice)
 
+
+# %%
+fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+helper.show_2d_image(phantom_recon_180_hamming_noise_30[0:125, 125, 0:125], fig, axes[0], None, None, None)
+helper.show_2d_image(phantom_recon_180_sart_noise_30[0:125, 125, 0:125], fig, axes[1], None, None, None)
+
+# %%
+helper.show_histogram(phantom_recon_180_hamming_noise_30, log=True)
+helper.show_histogram(phantom_recon_180_sart_noise_30, log=True)
+
+# %%
+recon_shape, recon_min, recon_max, recon_mean, recon_std = helper.get_stats(phantom_recon_180_hamming_noise_30)
+recon_sart_shape, recon_sart_min, recon_sart_max, recon_sart_mean, recon_sart_std = helper.get_stats(phantom_recon_180_sart_noise_30)
+
+# %%
+helper.plot_column(phantom_recon_180_hamming_noise_30)
+helper.plot_column(phantom_recon_180_sart_noise_30)
+
+# %%
+bin_recon = helper.binarize_image(phantom_recon_180_hamming_noise_30)
+bin_recon_sart = helper.binarize_image(phantom_recon_180_sart_noise_30)
+
+# %%
+helper.show_2d_sections(bin_recon, x=x_slice, y=y_slice, z=z_slice)
+helper.show_2d_sections(bin_recon_sart, x=x_slice, y=y_slice, z=z_slice)
+
+# %%
+recon_fs = helper.get_floating_solids(bin_recon)
+recon_sart_fs = helper.get_floating_solids(bin_recon_sart)
+helper.show_2d_sections(recon_fs, x=x_slice, y=y_slice, z=z_slice)
+helper.show_2d_sections(recon_sart_fs, x=x_slice, y=y_slice, z=z_slice)
 
 # %%
