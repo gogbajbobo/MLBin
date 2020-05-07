c75166de555f2ad70f10d831ddff96f8d464bd45
gogbajbobo <grigoriev.maxim@gmail.com>
2020-05-07 16:47:14 +0300

    Create ml_bin_v2.py

---
+++ ml_bin_v2.py
@@ -0,0 +1,30 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import helper
+import anisotropic_volume_generator as avg
+
+# %%
+size = 250
+dim = 3
+shape = tuple(size for _ in range(dim))
+phantom = avg.blobs(shape=shape, porosity=0.5, random_seed=3)
+
+# %%
