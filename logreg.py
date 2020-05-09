8ceaabb10e35a42b98fc7d21e1201b8386bf71bb
gogbajbobo <grigoriev.maxim@gmail.com>
2020-05-09 15:57:40 +0300

    return x_train, y_train

--- logreg.py
+++ logreg.py
@@ -15,7 +15,7 @@ def train_logreg_model(pixel_values, neighbor_average, pixel_bin_values):
     train_scaler.transform(x_train)
     logreg = LogisticRegression(C=1, solver='lbfgs').fit(x_train, y_train)
     print(f'mean: {train_scaler.mean_}, var: {train_scaler.var_}, samples_seen: {train_scaler.n_samples_seen_}')
-    return logreg
+    return logreg, x_train, y_train
 
 
 
