d0c2139343f26f15de775f134f2e762dc108ba3f
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-11 11:29:14 +0300

    Create interactive plot.py

---
+++ interactive plot.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %matplotlib widget
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+# %%
+x = np.arange(100)
+y = np.power(x, 2)
+
+plt.plot(x, y)
+
+# %%
